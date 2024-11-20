2831c3afc22c6f95aff0f405295a20472d6aed5c
m0rphine1 <107500228+m0rphine1@users.noreply.github.com>
2024-11-20 19:18:29 +0300

    Add files via upload

--- utils.hpp
+++ utils.hpp
@@ -6,9 +6,24 @@
 #include <iostream>
 #include <LIEF/LIEF.hpp>
 #include <unordered_set>
+#include <regex>
 
 using namespace LIEF;
 using namespace LIEF::PE;
+
+class param_t 
+{
+public:
+    param_t(std::string i_str, uint64_t i_addr, uint32_t i_offset)
+    {
+        str = i_str;
+        addr = i_addr;
+        offset = i_offset;
+    }
+    std::string str;
+    uint64_t addr;
+    uint32_t offset;
+};
 
 class obfuscated_import_t
 {
@@ -16,6 +31,11 @@ public:
     uint64_t entry = 0;
     uint64_t exit = 0;
     uint64_t api = 0;
+
+    uint64_t param_entry = 0;
+    uint64_t param_exit = 0;
+    x86_reg call_reg;
+    std::vector<param_t> params;
 
     uint64_t offset = 0;
     uint64_t prime = 0;
@@ -57,23 +77,32 @@ std::vector<obfuscated_import_t> FindObfuscatedImports(uint8_t* address, uint64_
             if (strcmp(insn[i].mnemonic, "mov") == 0)
             {
                 auto operands = insn[i].detail->x86.operands;
-                if (operands[0].type == X86_OP_REG && operands[1].type == X86_OP_MEM && operands[1].mem.disp == 0x60)
+                if (operands[0].type == X86_OP_REG && (operands[0].reg == X86_REG_RAX || operands[0].reg == X86_REG_RCX) &&
+                    operands[1].type == X86_OP_MEM && operands[1].mem.disp == 0x60)
                 {
                     obfuscated_import_t obfuscated_import;
                     obfuscated_import.entry = insn[i].address;
                     std::cout << "entry: 0x" << std::hex << insn[i].address << " ";
                     int stage = 0;
-
+                    
                     x86_reg calc_reg = X86_REG_INVALID;
+                    x86_reg add_reg = X86_REG_INVALID;
                     for (int j = i; j < count; j++)
                     {
                         auto operands = insn[j].detail->x86.operands;
 
                         if (strcmp(insn[j].mnemonic, "mov") == 0)
                         {
-                            if (insn[j].detail->x86.op_count && operands[0].type == X86_OP_REG &&
+                            if (insn[j].detail->x86.op_count && operands[1].type == X86_OP_MEM &&
+                                operands[0].type == X86_OP_REG && insn[j].detail->x86.operands[1].mem.disp == 0x30
+                                && stage == 0)
+                            {
+                                add_reg = operands[0].reg;
+                                ++stage;
+                            }
+                            else if (insn[j].detail->x86.op_count && operands[0].type == X86_OP_REG &&
                                 (operands[0].reg == X86_REG_EDX || operands[0].reg == X86_REG_R8D)
-                                && operands[1].type == X86_OP_IMM && stage == 0)
+                                && operands[1].type == X86_OP_IMM && stage == 1)
                             {
                                 calc_reg = operands[0].reg;
                                 std::cout << "offset: " << std::hex << operands[1].imm << " ";
@@ -85,7 +114,7 @@ std::vector<obfuscated_import_t> FindObfuscatedImports(uint8_t* address, uint64_
                         {
                             if (insn[j].detail->x86.op_count && operands[0].type == X86_OP_REG &&
                                 operands[0].reg == calc_reg &&
-                                operands[2].type == X86_OP_IMM && stage == 1)
+                                operands[2].type == X86_OP_IMM && stage == 2)
                             {
                                 std::cout << "prime: " << std::hex << operands[2].imm << " ";
                                 obfuscated_import.prime = operands[2].imm;
@@ -96,37 +125,86 @@ std::vector<obfuscated_import_t> FindObfuscatedImports(uint8_t* address, uint64_
                         {
                             if (insn[j].detail->x86.op_count && operands[0].type == X86_OP_REG &&
                                 operands[0].reg == calc_reg && operands[1].type == X86_OP_IMM &&
-                                stage == 2)
+                                stage == 3)
                             {
                                 std::cout << "hash: " << std::hex << operands[1].imm << " ";
                                 obfuscated_import.hash = operands[1].imm;
                                 ++stage;
                             }
                         }
-                        else if (strcmp(insn[j].mnemonic, "add") == 0)
+                        else if (strcmp(insn[j].mnemonic, "jmp") == 0)
                         {
-                            if (insn[j].detail->x86.op_count && operands[0].type == X86_OP_REG &&
-                                (operands[0].reg == X86_REG_RAX || operands[0].reg == X86_REG_R9) &&
-                                (operands[1].reg == X86_REG_R10 || operands[1].reg == X86_REG_R11)
-                                && stage == 3)
+                            if (insn[j].detail->x86.op_count && operands[0].type == X86_OP_IMM && stage == 4)
                             {
-                                if (strcmp(insn[j - 2].mnemonic, "jmp") != 0)
+                                bool x24_flag = false;
+                                bool x1c_flag = false;
+
+                                for (int l = j; l < count; l++)
                                 {
-                                    std::cout << "exit: 0x" << std::hex << insn[j + 1].address << " ";
-                                    obfuscated_import.exit = insn[j + 1].address;
-                                    ++stage;
+                                    if (obfuscated_import.param_entry == 0)
+                                    {
+                                        if (strcmp(insn[l].mnemonic, "call") == 0)
+                                        {
+                                            obfuscated_import.param_entry = insn[j].address;
+                                            obfuscated_import.param_exit = insn[l].address;
+                                            obfuscated_import.call_reg = insn[l].detail->x86.operands[0].reg;
+                                            l = j;
+                                        }
+                                    }
+                                    else if (insn[l].address < obfuscated_import.param_exit)
+                                    {
+                                        std::string insn_full = std::string(insn[l].mnemonic) + " " + std::string(insn[l].op_str);
+                                        std::unordered_set<uint64_t> not_params;
+
+                                        if (strcmp(insn[l].mnemonic, "mov") == 0 && insn[l].detail->x86.operands[0].type == X86_OP_REG &&
+                                            insn[l].detail->x86.operands[1].type == X86_OP_MEM && insn[l].detail->x86.operands[1].mem.disp == 0x24)
+                                        {
+                                            x24_flag = true;
+                                        }
+                                        else if (strcmp(insn[l].mnemonic, "movzx") == 0 && insn[l].detail->x86.operands[0].type == X86_OP_REG &&
+                                            insn[l].detail->x86.operands[1].type == X86_OP_MEM && insn[l].detail->x86.operands[1].mem.scale == 2 &&
+                                            x24_flag);
+                                        else if (strcmp(insn[l].mnemonic, "mov") == 0 && insn[l].detail->x86.operands[0].type == X86_OP_REG &&
+                                            insn[l].detail->x86.operands[1].type == X86_OP_MEM && insn[l].detail->x86.operands[1].mem.disp == 0x1C)
+                                        {
+                                            x1c_flag = true;
+                                        }
+                                        else if (strcmp(insn[l].mnemonic, "mov") == 0 && insn[l].detail->x86.operands[0].type == X86_OP_REG &&
+                                            insn[l].detail->x86.operands[1].type == X86_OP_MEM && insn[l].detail->x86.operands[1].mem.scale == 4 &&
+                                            x1c_flag);
+                                        else if (strcmp(insn[l].mnemonic, "add") == 0 && insn[l].detail->x86.operands[0].type == X86_OP_REG &&
+                                            insn[l].detail->x86.operands[1].type == X86_OP_REG && insn[l].detail->x86.operands[1].reg == add_reg);
+                                        else
+                                        {
+                                            uint64_t param_t_addr = 0;
+                                            uint64_t param_t_offset = 0;
+                                            for (int op_index = 0; op_index < insn[l].detail->x86.op_count; op_index++)
+                                            {
+                                                if (insn[l].detail->x86.operands[op_index].type == X86_OP_MEM && insn[l].detail->x86.operands[op_index].mem.base == X86_REG_RIP)
+                                                {
+                                                    param_t_addr = insn[l].address;
+                                                    param_t_offset = insn[l].detail->x86.operands[op_index].mem.disp;
+                                                }
+                                            }
+
+                                            obfuscated_import.params.push_back(param_t(insn_full, param_t_addr, param_t_offset));
+                                        }
+                                    }
                                 }
+                                std::cout << "exit: 0x" << std::hex << insn[j + 1].address << " ";
+                                ++stage;
                             }
                         }
                         else if (strcmp(insn[j].mnemonic, "call") == 0)
                         {
                             if (insn[j].detail->x86.op_count && operands[0].type == X86_OP_REG &&
-                                stage == 4)
+                                stage == 5)
                             {
                                 if (operands[0].reg != X86_REG_RAX)
                                     obfuscated_import.requires_manual = true;
 
                                 std::cout << "call: 0x" << std::hex << insn[j].address << std::endl;
+                                obfuscated_import.exit = insn[j].address;
 
                                 ++stage;
                                 imports.push_back(obfuscated_import);
@@ -157,59 +235,6 @@ uint32_t hash(const std::string s, uint32_t offset, uint32_t prime)
         h &= 0xFFFFFFFF; // keep only lower 32 bits
     }
     return h;
-}
-
-uint64_t ExtractApiAddress(uint64_t iat_ptr, std::unique_ptr<PE::Binary>& binary)
-{
-    uint64_t iat_address = *(uint64_t*)binary->get_content_from_virtual_address(iat_ptr, 8).data();
-    auto test = binary->get_content_from_virtual_address(iat_ptr, 8);
-    test.size();
-
-    csh handle;
-    cs_opt_mem mem_options;
-    mem_options.malloc = malloc;
-    mem_options.calloc = calloc;
-    mem_options.realloc = realloc;
-    mem_options.free = free;
-    mem_options.vsnprintf = vsnprintf;
-
-    if (cs_option(NULL, CS_OPT_MEM, (size_t)&mem_options) != CS_ERR_OK)
-    {
-        return 0;
-    }
-
-    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
-        std::cerr << "Capstone initialization failed!" << std::endl;
-        return 0;
-    }
-
-    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
-
-    cs_insn* insns;
-    size_t count = cs_disasm(handle, binary->get_content_from_virtual_address(iat_address,0xE).data(), 0xE, (uint64_t)iat_address, 0, &insns);
-
-    int base_addr = 0;
-    int offset = 0;
-
-    for (int i = 0; i < count; i++)
-    {
-        cs_insn insn = insns[i];
-        if (strcmp(insn.mnemonic, "call") == 0)
-        {
-            continue;
-        }
-        else if (strcmp(insn.mnemonic, "pop") && base_addr == 0)
-        {
-            base_addr = insn.address;
-        }
-        else if (strcmp(insn.mnemonic, "add") && base_addr != 0)
-        {
-            offset = insn.detail->x86.operands[1].imm;
-            break;
-        }
-    }
-
-    return base_addr + offset;
 }
 
 void ResolveObfuscatedImports(std::vector<obfuscated_import_t>& obfuscated_imports, std::unique_ptr<PE::Binary>& binary)
@@ -269,4 +294,35 @@ void AddFunctions(std::string dll_name, std::unique_ptr<PE::Binary>& binary)
             import_dll.add_entry(export_entry.name());
         }
     }
+}
+
+
+std::string ModifyRipOffset(const std::string& asm_line, int new_offset)
+{
+    // Geliþtirilmiþ regex deseni: "[rip + ..." kýsmýný tüm baðlamýyla eþleþtirir.
+    std::regex rip_pattern(R"(\[rip\s*\+\s*0x([0-9A-Fa-f]+)\])");
+
+    // "[rip + ..." kýsmýný arýyoruz
+    if (std::regex_search(asm_line, rip_pattern)) {
+        std::smatch match;
+        std::regex_search(asm_line, match, rip_pattern);
+
+        // Eski offset'i al
+        std::string old_offset_str = match[1].str();
+
+        // Offset'i integer olarak çöz
+        int old_offset = std::stoi(old_offset_str, nullptr, 16);
+
+        // Yeni offset'i string olarak yaz
+        std::stringstream new_offset_stream;
+        new_offset_stream << std::hex << new_offset;
+        std::string new_offset_str = new_offset_stream.str();
+
+        // Eski offset kýsmýný yeni offset ile deðiþtir
+        std::string modified_asm = std::regex_replace(asm_line, rip_pattern, "[rip + 0x" + new_offset_str + "]");
+        return modified_asm;
+    }
+
+    // Eðer "[rip + ...]" bulunmazsa, orijinal metni döndür
+    return asm_line;
 }
